e5adf6f72caab2a6e3bff90d1aade0f57fad792c
Maxime22 <sabouretmaxime@gmail.com>
2024-11-11 16:44:24 +0100

    tests: add other tests than nominal case

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -32,4 +32,157 @@ class ExampleTest extends TestCase
 
         $this->assertSame($expectedOutput, $result);
     }
+
+    public function testStatementWithLowAudience()
+    {
+        // Arrange
+        $sut = new Example();
+        $invoice = [
+            "customer" => "SmallCo",
+            "performances" => [
+                ["playID" => "hamlet", "audience" => 5],
+                ["playID" => "as-like", "audience" => 15],
+                ["playID" => "othello", "audience" => 10]
+            ]
+        ];
+        $plays = [
+            "hamlet" => ["name" => "Hamlet", "type" => "tragedy"],
+            "as-like" => ["name" => "As You Like It", "type" => "comedy"],
+            "othello" => ["name" => "Othello", "type" => "tragedy"]
+        ];
+
+        // Act
+        $result = $sut->statement($invoice, $plays);
+
+        $expectedOutput = "Statement for SmallCo<br>" .
+                        "Hamlet: $400.00 (5 seats)<br>" .
+                        "As You Like It: $345.00 (15 seats)<br>" .
+                        "Othello: $400.00 (10 seats)<br>" .
+                        "Amount owed is $1,145.00<br>" .
+                        "You earned 3 credits<br>";
+
+        // Assert
+        $this->assertSame($expectedOutput, $result);
+    }
+
+    public function testStatementWithAudienceZero()
+    {
+        // Arrange
+        $sut = new Example();
+        $invoice = [
+            "customer" => "LowAudienceCo",
+            "performances" => [
+                ["playID" => "hamlet", "audience" => 0]
+            ]
+        ];
+        $plays = [
+            "hamlet" => ["name" => "Hamlet", "type" => "tragedy"]
+        ];
+
+        // Act
+        $result = $sut->statement($invoice, $plays);
+        $expectedOutput = "Statement for LowAudienceCo<br>" .
+                        "Hamlet: $400.00 (0 seats)<br>" .
+                        "Amount owed is $400.00<br>" .
+                        "You earned 0 credits<br>";
+
+        // Assert
+        $this->assertSame($expectedOutput, $result);
+    }
+
+    public function testStatementWithAudienceAtThresholds()
+    {
+        // Arrange
+        $sut = new Example();
+        $invoice = [
+            "customer" => "ThresholdCo",
+            "performances" => [
+                ["playID" => "hamlet", "audience" => 30],
+                ["playID" => "as-like", "audience" => 20]
+            ]
+        ];
+        $plays = [
+            "hamlet" => ["name" => "Hamlet", "type" => "tragedy"],
+            "as-like" => ["name" => "As You Like It", "type" => "comedy"]
+        ];
+
+        // Act
+        $result = $sut->statement($invoice, $plays);
+        $expectedOutput = "Statement for ThresholdCo<br>" .
+                        "Hamlet: $400.00 (30 seats)<br>" .
+                        "As You Like It: $360.00 (20 seats)<br>" .
+                        "Amount owed is $760.00<br>" .
+                        "You earned 4 credits<br>";
+
+        // Assert
+        $this->assertSame($expectedOutput, $result);
+    }
+
+    public function testStatementWithUnknownPlayType()
+    {
+        // Assert
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("Unknown type mystery");
+
+        // Arrange
+        $sut = new Example();
+        $invoice = [
+            "customer" => "UnknownPlayTypeCo",
+            "performances" => [
+                ["playID" => "mystery-play", "audience" => 20]
+            ]
+        ];
+        $plays = [
+            "mystery-play" => ["name" => "Mystery Play", "type" => "mystery"]
+        ];
+
+        // Act
+        $sut->statement($invoice, $plays);
+    }
+
+    public function testStatementWithNoPerformances()
+    {
+        // Arrange
+        $sut = new Example();
+        $invoice = [
+            "customer" => "EmptyCo",
+            "performances" => []
+        ];
+        $plays = [];
+
+        // Act
+        $result = $sut->statement($invoice, $plays);
+        $expectedOutput = "Statement for EmptyCo<br>" .
+                        "Amount owed is $0.00<br>" .
+                        "You earned 0 credits<br>";
+
+        // Assert
+        $this->assertSame($expectedOutput, $result);
+    }
+
+    public function testStatementWithNegativeAudience()
+    {
+        // Arrange
+        $sut = new Example();
+        $invoice = [
+            "customer" => "NegativeAudienceCo",
+            "performances" => [
+                ["playID" => "hamlet", "audience" => -5]
+            ]
+        ];
+        $plays = [
+            "hamlet" => ["name" => "Hamlet", "type" => "tragedy"]
+        ];
+
+        // Act
+        $result = $sut->statement($invoice, $plays);
+        $expectedOutput = "Statement for NegativeAudienceCo<br>" .
+                        "Hamlet: $400.00 (-5 seats)<br>" .
+                        "Amount owed is $400.00<br>" .
+                        "You earned 0 credits<br>";
+
+        // Assert
+        $this->assertSame($expectedOutput, $result);
+    }
+
 }
